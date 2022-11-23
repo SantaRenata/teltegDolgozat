f17a50e3991e7e1cdd7ae055f32f0d578df9198d
SantaRenata <renatasanta2002@gmail.com>
2022-11-23 16:55:58 +0100

    Weboldal formázva, fejléc pótolva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,13 @@
+/*
+* File: app.component.ts
+* Author: Sánta Renáta Diána
+* Copyright: 2022, Sánta Renáta Diána
+* Group: Szoft II N
+* Date: 2022-11-23
+* Github: https://github.com/SantaRenata/
+* Licenc: GNU GPL
+*/
+
 import { Component } from '@angular/core';
 
 @Component({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,13 @@
+/*
+* File: app.module.ts
+* Author: Sánta Renáta Diána
+* Copyright: 2022, Sánta Renáta Diána
+* Group: Szoft II N
+* Date: 2022-11-23
+* Github: https://github.com/SantaRenata/
+* Licenc: GNU GPL
+*/
+
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
